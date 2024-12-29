c46c183983967f8f7d6bf216e317da48b29dfe04
fedichv <fedichev06@bk.ru>
2024-12-29 17:00:10 +0300

    created base project, removed storyboard

---
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Владимир Федичев on 29.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
