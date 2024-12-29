1376bc79a5dacd24ce28010b0e3af7249c22df6f
fedichv <fedichev06@bk.ru>
2024-12-29 18:25:20 +0300

    added new base colors

---
+++ FoodDeliveryApp/FoodDeliveryApp/Resources/AppColors.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+enum AppColors {
+    static let accentOrange = UIColor.hex(0xD35400)
+    static let grey = UIColor.hex(0xECF0F1)
+    static let black = UIColor.hex(0x000000)
+    static let background = UIColor.hex(0xFFFFFF)
+}
