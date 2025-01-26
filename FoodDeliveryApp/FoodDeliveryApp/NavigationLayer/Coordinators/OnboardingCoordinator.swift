a3d3412414e6465a1f8391015522c63f8f2665c9
fedichv <fedichev06@bk.ru>
2025-01-26 21:51:39 +0300

    bug fix

--- FoodDeliveryApp/FoodDeliveryApp/NavigationLayer/Coordinators/OnboardingCoordinator.swift
+++ FoodDeliveryApp/FoodDeliveryApp/NavigationLayer/Coordinators/OnboardingCoordinator.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
+// MARK: -OnboardingCoordinator
 class OnboardingCoordinator: Coordinator {
     
+    // MARK: - Propeerties
+    private let factory = SceneFactory.self
+    
+    // MARK: - Methods
     override func start() {
         let vc = ViewController()
         navigationController?.pushViewController(vc, animated: true)
     }
     override func finish() {
         print("AppCoordinaror finish")
+        finishDelegate?.coordinatorDidFinish(childCoordinates: self)
+    }
+}
+
+// MARK: - Navigation
+private extension OnboardingCoordinator {
+    func showOnboarding() {
+        let viewController = factory.makeOnboardingScene(coordinator: self)
+        navigationController?.pushViewController(viewController, animated: true)
+        
     }
 }
